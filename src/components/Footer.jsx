3fcaf1c7a2a9e660419bd776a781059528ba352a
Nex.Dev <nezerekunke@gmail.com>
2025-04-12 19:07:20 +0100

    Finalist

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ export default function Footer () {
 
     return(
         <div className='attribution'>
-            Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
-            Coded by <a href="https://github.com/dBillionaireDev">Nexy(d'BillionaireDev)👨🏽‍💻</a>
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+            Coded by <a href="https://github.com/dBillionaireDev" target="_blank">Nexy(d'BillionaireDev)👨🏽‍💻</a>
         </div>
     )
 }
